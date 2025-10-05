bcf3950a0cb716e7ab5082234384a22a90a8cd69
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2025-10-06 01:31:22 +0530

    Update zsh setup script to comment out commands
    Comment out installation and shell change commands for zsh.

--- .config/zsh-setup.sh
+++ .config/zsh-setup.sh
@@ -1,11 +1,14 @@
+######## Uncomment if zsh is not installed ###########################
+######################################################################
 # Install zsh
-sudo apt install zsh -y
+# sudo apt install zsh -y
 
-# Change shell to zsh
-sudo chsh -s $(which zsh)
+# # Change shell to zsh
+# sudo chsh -s $(which zsh)
 
-# Restart shell
-exec zsh
+# # Restart shell
+# exec zsh
+######################################################################
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -13,27 +16,23 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Edit .zshrc file to use the Powerlevel10k theme
-sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
-
-
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone --depth=1 https://github.com/hlissner/zsh-autopair.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autopair
 
-# Edit .zshrc file to use the zsh-autopair plugin
-sed -i 's/plugins=(.*)/plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-autopair)/' ~/.zshrc
+# Edit .zshrc file
+sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+sed -i '1i # Set up brew\neval "$(brew shellenv)"' ~/.zshrc
+sed -i "/^[^#]*plugins=/s/plugins=(.*)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-autopair)\n\n# Set up brew\neval \"\$(brew shellenv)\"\n/" ~/.zshrc
 
 # Install necessary dev tool dependency
-sudo apt install fzf fd-find ripgrep bat -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-sudo ln -s $(which fdfind) /usr/bin/fd
+sudo brew install bat fd fzf jq ripgrep yazi zoxide font-symbols-only-nerd-font
 
 echo "
-[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
-[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 # FZF default options (colors, layout, etc.)
 export FZF_DEFAULT_OPTS='
   --height 80%
@@ -41,16 +40,28 @@ export FZF_DEFAULT_OPTS='
   --border
 '
 export FZF_CTRL_T_OPTS='
---preview=\"batcat --style=numbers --color=always {} || cat {}\"
+--preview=\"bat --style=numbers --color=always {} || cat {}\"
 --preview-window=right:60%
 '
 # Optional: Use fd (if installed) for faster file search
 export FZF_DEFAULT_COMMAND='fd --type f --exclude \".git\" --exclude \"node_modules\" --exclude \".venv\" --exclude \"venv\"'
 export FZF_CTRL_T_COMMAND=\"\$FZF_DEFAULT_COMMAND\"
+
+# Setup zoxide
+eval \"\$(zoxide init zsh)\"
+# Setup fzf
+source <(fzf --zsh)
+# Setup yazi
+function y() {
+	local tmp=\"\$(mktemp -t \"yazi-cwd.XXXXXX\")\" cwd
+	yazi \"\$@\" --cwd-file=\"\$tmp\"
+	IFS= read -r -d '' cwd < \"\$tmp\"
+	[ -n \"\$cwd\" ] && [ \"\$cwd\" != \"\$PWD\" ] && builtin cd -- \"\$cwd\"
+	rm -f -- \"\$tmp\"
+}
 " >> ~/.zshrc
 
-echo "alias bat=batcat
-alias python=python3
+echo "alias python=python3
 " >> ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/alias.zsh
 
 source ~/.zshrc
